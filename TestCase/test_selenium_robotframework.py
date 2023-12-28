0286f84184c281c330eea9fac093e80cea83971f
nicktest01 <nicktest003@gmail.com>
2023-12-28 18:57:22 +0800

    robot demo

--- TestCase/test_selenium_robotframework.py
+++ TestCase/test_selenium_robotframework.py
@@ -1,6 +1,12 @@
 # -*- coding:utf-8 -*-
+
+
 *** Test Cases ***    
 Open Google and Search Nicktest
+    show robot test
+
+*** Keywords ***    
+show robot test
     Open Browser    https://www.google.com    chrome
     Input Text      name    # 找到搜尋框並輸入 "nicktest"
     Press Keys      name    # 模擬按下 Enter 鍵
